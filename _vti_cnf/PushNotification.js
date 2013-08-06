a3992b65276a8079d87991e1854880c951246222
Michael Tigue <mtigue@web4uonline.com>
2013-08-05 20:15:56 -0400

    8

---
+++ _vti_cnf/PushNotification.js
@@ -0,0 +1,12 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|06 Aug 2013 00:07:51 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|web4u\\mtigue
+vti_modifiedby:SR|web4u\\mtigue
+vti_timecreated:TR|03 Jun 2013 11:31:37 -0000
+vti_syncofs_68.178.246.6\:21/pdxvocations:TW|06 Aug 2013 00:08:33 -0000
+vti_syncwith_68.178.246.6\:21/pdxvocations:TX|06 Aug 2013 00:07:51 -0000
+vti_cacheddtm:TX|06 Aug 2013 00:07:51 -0000
+vti_filesize:IR|45099
+vti_nexttolasttimemodified:TR|03 Jun 2013 11:31:37 -0000
+vti_backlinkinfo:VX|index3.html index-perfect-ios.html index-backup.html 19/main.html 19/main-backup.html
---
+++ assets/js/_vti_cnf/apprise-1.5.full.js
@@ -0,0 +1,12 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|web4u\\mtigue
+vti_modifiedby:SR|web4u\\mtigue
+vti_timelastmodified:TR|06 Aug 2013 00:10:00 -0000
+vti_timecreated:TR|23 Jun 2013 19:02:13 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_nexttolasttimemodified:TR|05 Aug 2013 18:17:37 -0000
+vti_syncofs_68.178.246.6\:21/pdxvocations:TW|05 Aug 2013 18:36:24 -0000
+vti_syncwith_68.178.246.6\:21/pdxvocations:TX|23 Jun 2013 19:02:52 -0000
+vti_cacheddtm:TX|06 Aug 2013 00:09:58 -0000
+vti_filesize:IR|3550
+vti_backlinkinfo:VX|14/main.html
