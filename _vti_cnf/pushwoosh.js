b29f5de509285cb96bd78ec6f2c0d1c4f40b6456
Michael Tigue <mtigue@web4uonline.com>
2013-08-06 00:26:29 -0400

    21

--- _vti_cnf/pushwoosh.js
+++ _vti_cnf/pushwoosh.js
@@ -6,6 +6,6 @@ vti_modifiedby:SR|web4u\\mtigue
 vti_timecreated:TR|02 Jun 2013 21:32:29 -0000
 vti_cacheddtm:TX|02 Jun 2013 21:32:29 -0000
 vti_filesize:IR|3450
-vti_backlinkinfo:VX|index3.html 19/index.html index-perfect-ios.html index-backup.html 19/main-backup.html
+vti_backlinkinfo:VX|index3.html index-perfect-ios.html index-backup.html 19/main-backup.html
 vti_syncofs_68.178.246.6\:21/pdxvocations:TW|21 Jul 2013 23:24:19 -0000
 vti_syncwith_68.178.246.6\:21/pdxvocations:TX|02 Jun 2013 21:32:29 -0000
